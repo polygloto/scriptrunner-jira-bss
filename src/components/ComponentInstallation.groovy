0eb74aa2dc679d4747aaf3daf332a45dc0f1f999
polygloto <polygloto@mail.ru>
2021-04-15 18:28:28 +0300

    Added getting matches from the database.

--- src/components/ComponentInstallation.groovy
+++ src/components/ComponentInstallation.groovy
@@ -6,31 +6,52 @@ import com.atlassian.jira.event.type.EventDispatchOption
 import com.atlassian.jira.issue.Issue
 import com.atlassian.jira.issue.IssueInputParametersImpl
 import com.atlassian.jira.issue.label.Label
-
-// script settings
-String labelDefault = "test-label"
-String labelZup = "test-label-zup"
-String componentDefault = "test component"
-String componentZup = "test component zup"
+import com.onresolve.scriptrunner.db.DatabaseUtil
 
 def issue = event.issue as Issue
 def project = issue.getProjectObject()
 def projectComponentManager = ComponentAccessor.getProjectComponentManager()
-def component
-List<String> stringLabelList = new ArrayList<>()
+def labels = issue.getLabels()
 
-for (Label label : issue.getLabels()) {
-    stringLabelList.add(label.toString())
+if (issue.getComponents().size() > 0){
+    log.warn('The component (${issue.getComponents().size().toString()}) is already installed, the script is stopped.')
+    return
 }
 
-if (stringLabelList.contains(labelZup)) {
-    component = projectComponentManager.findByComponentName(project.getId(), componentZup)
-    log.warn('setting zup component')
-}else if (stringLabelList.contains(labelDefault)){
-    component = projectComponentManager.findByComponentName(project.getId(), componentDefault)
-    log.warn('setting default component')
-}else {
-    log.warn('labels not found in script settings')
+if (labels.size() > 1) {
+    log.warn('There is more than one label in the issue, the component is not set.')
+    return
+} else if (labels.size() == 0) {
+    log.warn('There are no labels in the task, the component is not set.')
+    return
+}
+
+def stringLabel = labels[0].toString()
+
+def queryResult = DatabaseUtil.withSql('map_users_db') { sql ->
+    sql.rows('SELECT jira_component FROM map_users_table WHERE jira_label = \'' + stringLabel + '\'')
+}
+
+if (queryResult.size() > 1) {
+    log.warn('There is more than one component in database, the component is not set.')
+    return
+} else if (queryResult.size() == 0) {
+    log.warn('There is no component for label ${stringLabel} in database, the component is not set.')
+    return
+}
+
+String componentName = queryResult[0].jira_component
+
+if (componentName != null){
+    log.warn('Received a component from the database successfully. Component name = '.concat(componentName))
+}else{
+    log.warn('Failed to get component from database. Column jira_component = null')
+}
+
+def component = projectComponentManager.findByComponentName(project.getId(), componentName)
+
+if (component == null){
+    log.warn('The component ${componentName} was not found in the project ${project.name.toString()}')
     return
 }
 
@@ -39,5 +60,13 @@ def issueService = ComponentAccessor.getIssueService()
 
 def issueInputParams = new IssueInputParametersImpl().setComponentIds(component?.getId())
 IssueService.UpdateValidationResult updateValidationResult = issueService.validateUpdate(user, issue.id, issueInputParams)
+
 // update the issue but do not dispatch any event or send notification
 issueService.update(user, updateValidationResult, EventDispatchOption.DO_NOT_DISPATCH, false)
+
+// set assignee
+//TODO Validation ERROR
+def componentLead = component.getComponentLead()
+def validateAssignResult = issueService.validateAssign(componentLead, issue.id, issue.reporterId)
+issueService.assign(componentLead, validateAssignResult)
+log.warn('Installed assignee ${componentLead.toString()}')
