1fb4f6e1d30a85399936bbbdf49073f32e231cf3
polygloto <polygloto@mail.ru>
2021-04-16 12:30:50 +0300

    Setting the assignee from the database

--- src/components/ComponentInstallation.groovy
+++ src/components/ComponentInstallation.groovy
@@ -14,8 +14,10 @@ def project = issue.getProjectObject()
 def projectComponentManager = ComponentAccessor.getProjectComponentManager()
 def labels = issue.getLabels()
 
-if (issue.getComponents().size() > 0){
-    log.warn('The component (${issue.getComponents().size().toString()}) is already installed, the script is stopped.')
+boolean componentIsEmpty = issue.components.empty
+
+if (!componentIsEmpty){
+    log.warn('The component is already installed, the script is stopped.')
     return
 }
 
@@ -30,18 +32,19 @@ if (labels.size() > 1) {
 def stringLabel = labels[0].toString()
 
 def queryResult = DatabaseUtil.withSql('map_users_db') { sql ->
-    sql.rows('SELECT jira_component FROM map_users_table WHERE jira_label = \'' + stringLabel + '\'')
+    sql.rows('SELECT jira_component, jira_user FROM map_users_table WHERE jira_label = \'' + stringLabel + '\'')
 }
 
 if (queryResult.size() > 1) {
     log.warn('There is more than one component in database, the component is not set.')
     return
 } else if (queryResult.size() == 0) {
-    log.warn('There is no component for label ${stringLabel} in database, the component is not set.')
+    log.warn('There is no component for label '.concat(stringLabel).concat(' in database, the component is not set.'))
     return
 }
 
 String componentName = queryResult[0].jira_component
+String jiraUserFromDb = queryResult[0].jira_user
 
 if (componentName != null){
     log.warn('Received a component from the database successfully. Component name = '.concat(componentName))
@@ -52,7 +55,7 @@ if (componentName != null){
 def component = projectComponentManager.findByComponentName(project.getId(), componentName)
 
 if (component == null){
-    log.warn('The component ${componentName} was not found in the project ${project.name.toString()}')
+    log.warn('The component '.concat(componentName).concat(' was not found in the project '.concat(project.name)))
     return
 }
 
@@ -66,7 +69,20 @@ IssueService.UpdateValidationResult updateValidationResult = issueService.valida
 issueService.update(user, updateValidationResult, EventDispatchOption.DO_NOT_DISPATCH, false)
 
 // set assignee
-ApplicationUser componentLead = component.getComponentLead()
-def validateAssignResult = issueService.validateAssign(componentLead, issue.id, componentLead.username)
-issueService.assign(componentLead, validateAssignResult)
-log.warn('Set assignee {$componentLead.username}')
+ApplicationUser assignee
+String userSource = ""
+
+if (jiraUserFromDb != null){
+    def userManager = ComponentAccessor.getUserManager()
+    assignee = userManager.getUserByName(jiraUserFromDb)
+    userSource = "database"
+}
+
+if (assignee == null){
+    assignee = component.getComponentLead()
+    userSource = "component leader"
+}
+
+def validateAssignResult = issueService.validateAssign(assignee, issue.id, assignee.username)
+issueService.assign(assignee, validateAssignResult)
+log.warn('Set assignee '.concat(assignee.username).concat(' from '.concat(userSource)))
